307d9ec14b4def06d258de2d637cc08c57164dee
yousef khraiwesh <yu.yousseff@gmail.com>
2024-06-29 22:31:58 +0300

    Add files via upload

---
+++ 25.06/25.06/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Write("read line:");
+            // String UserInput = Console.ReadLine();
+
+            // Console.WriteLine("you put:" + UserInput);       
+            //  ///////////////////////////// 2
+
+
+            //double myDouble = 99.99;
+
+            //  string myString = "yousef kh";
+
+            // char myChar = 'y';
+
+            //  bool myBool = true;
+
+            // int myInt = 14/9;
+
+            //  const double myConstDouble = 4.9584;
+
+
+            //  Console.WriteLine("Double: " + myDouble);
+            //  Console.WriteLine("String: " + myString);
+            //  Console.WriteLine("Char: " + myChar);
+            //  Console.WriteLine("Bool: " + myBool);
+            //  Console.WriteLine("Int: " + myInt);
+            //  Console.WriteLine("Const Double: " + myConstDouble);
+
+            /////////////////// 3
+
+            //string[] cars = { "bmw", "maclaren", "audi", "dodge" };
+            //Console.WriteLine(" the cars in array are : " + cars.Length);
+            //Console.WriteLine(cars[0]);
+            //Console.WriteLine(cars[1]);
+            //Console.WriteLine(cars[2]);
+            //Console.WriteLine(cars[3]);
+
+            // ?طريقه جديده
+            //   Console.WriteLine("all the cars in array:");
+            //  foreach (string c in cars)
+            //    Console.WriteLine(c);
+
+            /////////////////// 4 
+
+            //Console.Write("Input your firstname:");
+            //string firstName = Console.ReadLine();
+
+            // Console.Write("Input your lastname:");
+            //string lastName = Console.ReadLine();
+
+            //Console.Write("Input your year of birth: ");
+            //String yearOfBirth = Console.ReadLine();
+
+            //Console.WriteLine(firstName + " " + lastName + " " + yearOfBirth);
+
+            /////////////////// 5 
+            //int[] numbers = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+
+            //numbers[0] = 1; // redeclea for value
+            //numbers[1] = 1;
+            //numbers[2] = 2;
+
+            //Console.WriteLine(numbers[0]); // print the integer =1
+            //Console.WriteLine(numbers[1]); // = 1
+            //Console.WriteLine(numbers[2]); // = 2
+
+            //Console.WriteLine(numbers.Length); // = 10 caractars
+
+            /////////////////// 6
+          //  copy i need to under stand it
+             int[] summation = { 2, 5, 8 };
+            int sumValue = summation[0] + summation[1] + summation[2];
+            Console.WriteLine("Sum of all elements stored in the array is : " + sumValue);
+
+        }
+    }
+    }
+    
+
---
+++ 26-6/26-6/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //////////////////////////////////////////////////////////// task day  2
+
+            // 1  Write a C# program that accept two integers and display the smaller.
+
+            //Console.Write("The smallest number: ");
+            //int[] numbers = { 14, 5 };
+            //Array.Sort(numbers);
+            //Console.WriteLine(numbers[0])                // = 5
+
+
+            ////  2   Write a C# conditional statement to find the sign  of input  number. Display sign.
+
+            //Console.Write("Enter a number:");
+            //int numberSign = Convert.ToInt32(Console.ReadLine());
+            //int sign = Math.Sign(numberSign);
+            //Console.WriteLine($"The sign of number: {sign}");            // = 0
+
+
+            ////  3  Write a C# conditional statement to sort three numbers ascending. Use console to display the result.
+            ///
+            //int[] numbers1 = { 0, -1, 4 };
+            //Array.Sort(numbers1);
+            //Console.WriteLine($"Sort three numbers ascending: {numbers1[0]} {numbers1[1]} {numbers1[2]} ");    // = { -1 , 0 , 4 }
+
+
+            ////  4  Write a C# conditional statement to find the max of five numbers. Use console to display the result
+
+            //int[] numbers = { 4, 9, -7, 15, 25 };
+            //int maxNum = numbers.Max();
+            //Console.WriteLine($"The max number is : {maxNum}");
+
+
+            ////  5   Write a C# program that converts kilometers per hour to miles per hour
+
+            //Console.Write("kilometers per hour: ");
+            //int Kilometer = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"miles per hour: {Kilometer * 0.621371192}");
+
+            ////  6  Write a C# program that takes hours and minutes as input and calculates the total number of minutes.
+
+            //Console.WriteLine("Please enter the hours : ");
+            //int int8 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter the minutes : ");
+            //int int9 = Convert.ToInt32(Console.ReadLine());
+            //int8 = int8 * 60;
+            //int sum = int8 + int9;
+            //Console.WriteLine($"The total minutes equal : {sum} ");
+
+
+            //// 7  Write a program in C# that takes minutes as input and display the total number of hours and minutes. 
+
+            //Console.WriteLine("Please enter the minutes : ");
+            //int int10= Convert.ToInt32(Console.ReadLine());
+            //int total = int10 / 60;
+            //int minutes = int10 % 60;
+            //Console.WriteLine($" {total} hours , {minutes} minutes ");
+
+
+            //  8  Create string of array that contains 5 element of different sentence with different length .Print all elements with fixed length for each sentence.
+
+            //string[] strings = { "Hello world!", "The life is nice", "How are you?", "Im here waiting for the lunch", "is the lunch ready? " };
+            //Console.WriteLine(strings[0] + "\n" +"The length is : " +  strings[0].Length);
+            //Console.WriteLine(strings[1] + "\n" + "The length is : " + strings[1].Length);
+            //Console.WriteLine(strings[2] + "\n" + "The length is : " + strings[2].Length);
+            //Console.WriteLine(strings[3] + "\n" + "The length is : " + strings[3].Length);
+            //Console.WriteLine(strings[4] + "\n" + "The length is : " + strings[4].Length);
+
+            ///////////////
+            ///
+
+            int i = 0;
+            while (i < 5) 
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            /////////
+
+            int k = 2;
+            do
+            {
+                Console.WriteLine(k);
+                k++;
+            }
+            while (k < 8);
+        }
+        
+    
+    }
+    }
+
