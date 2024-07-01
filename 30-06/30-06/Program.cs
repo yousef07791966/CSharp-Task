d564987f393be2f8205608b8721532833dc730d8
yousef khraiwesh <yu.yousseff@gmail.com>
2024-07-01 05:40:10 +0300

    Add files via upload

---
+++ 30-06/30-06/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_06
+{
+    
+    }
+    public class ahmad 
+    {
+        // Function to return the name
+        public static string GetMyName()
+        {
+            return "Yousef";
+        }
+
+        // Example usage
+        static void Main()
+        {
+            string myName = GetMyName();
+            Console.WriteLine($"My name is: {myName}");
+        }
+    }
+
